eed0ddd40b4bddc0cbb5aec9bac1b9cef2b44a61
Yury Frolov <yura200253@gmail.com>
2021-07-28 20:47:23 +0500

    Added test for writeIndexBatch

--- storage/clickhousespanstore/writer_test.go
+++ storage/clickhousespanstore/writer_test.go
@@ -76,3 +76,58 @@ func testSpanWriterWriteModelBatch(t *testing.T, encoding Encoding, marshal func
 
 	spyLogger.AssertLogsEmpty(t)
 }
+
+func TestSpanWriter_WriteIndexBatch(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	spyLogger := mocks.NewSpyLogger()
+	indexTable := "test_index_table"
+	spanWriter := NewSpanWriter(
+		spyLogger,
+		db,
+		indexTable,
+		"",
+		EncodingJSON,
+		0,
+		0,
+	)
+
+	mock.ExpectBegin()
+
+	spans := make([]*model.Span, 1000)
+	for i := 0; i < 1000; i++ {
+		process := model.Process{ServiceName: "service" + strconv.FormatUint(rand.Uint64(), 10)}
+		span := model.Span{
+			TraceID:       model.NewTraceID(rand.Uint64(), rand.Uint64()),
+			SpanID:        model.NewSpanID(rand.Uint64()),
+			OperationName: "operation" + strconv.FormatUint(rand.Uint64(), 10),
+			StartTime:     time.Unix(rand.Int63n(time.Now().Unix()), 0),
+			Process: &process,
+			Duration: time.Unix(rand.Int63n(1 << 32), 0).Sub(time.Unix(0, 0)),
+		}
+		spans[i] = &span
+
+		mock.ExpectExec(fmt.Sprintf(
+			"INSERT INTO %s INSERT INTO %s (timestamp, traceID, service, operation, durationUs, tags) VALUES (%s, %s, %s, %d, %s)",
+			indexTable,
+			span.StartTime,
+			span.TraceID,
+			span.Process.ServiceName,
+			span.OperationName,
+			span.Duration.Microseconds(),
+			uniqueTagsForSpan(&span),
+		))
+	}
+
+	mock.ExpectCommit()
+
+	if err = spanWriter.writeModelBatch(spans); err != nil {
+		t.Fatalf("Could not write spans due to error: %s", err)
+	}
+
+	spyLogger.AssertLogsEmpty(t)
+}

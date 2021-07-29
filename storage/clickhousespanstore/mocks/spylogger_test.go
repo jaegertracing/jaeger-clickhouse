4a7bac0a0a117672f388b2eded579ba35c10808e
Yury Frolov <yura200253@gmail.com>
2021-07-29 12:02:18 +0500

    More SpyLogger tests

--- storage/clickhousespanstore/mocks/spylogger_test.go
+++ storage/clickhousespanstore/mocks/spylogger_test.go
@@ -12,16 +12,44 @@ func TestSpyLogger_AssertLogsEmpty(t *testing.T) {
 	logger.AssertLogsEmpty(t)
 }
 
-func TestSpyLogger_AssertLogsOfLevelEqual(t *testing.T) {
+func TestSpyLogger_AssertLogsOfLevelEqualNoArgs(t *testing.T) {
 	logger := NewSpyLogger()
-	var logs = make([][]string, levelCount)
+	var logs = make([][]LogMock, levelCount)
 	for level, levelLogs := range logs {
 		logsCount := rand.Intn(80)
 		for i := 0; i < logsCount; i++ {
-			log := "log" + strconv.FormatUint(rand.Uint64(), 10)
-			levelLogs = append(levelLogs, log)
-			logger.Log(hclog.Level(level+1), log)
+			msg := "msg" + strconv.FormatUint(rand.Uint64(), 10)
+			levelLogs = append(levelLogs, LogMock{Msg: msg})
+			logger.Log(hclog.Level(level+1), msg)
 		}
+		logs[level] = levelLogs
+	}
+
+	for level, levelLogs := range logs {
+		logger.AssertLogsOfLevelEqual(t, hclog.Level(level+1), levelLogs)
+	}
+}
+
+func TestSpyLogger_AssertLogsOfLevelEqualArgs(t *testing.T) {
+	logger := NewSpyLogger()
+	var logs = make([][]LogMock, levelCount)
+	for level, levelLogs := range logs {
+		logsCount := rand.Intn(80)
+		for i := 0; i < logsCount; i++ {
+			msg := "msg" + strconv.FormatUint(rand.Uint64(), 10)
+			argsCount := rand.Intn(10)
+			args := make([]interface{}, 0, 2*argsCount)
+			for j := 0; j < argsCount; j++ {
+				args = append(
+					args,
+					"key"+strconv.FormatUint(rand.Uint64(), 10),
+					"value"+strconv.FormatUint(rand.Uint64(), 10),
+				)
+			}
+			levelLogs = append(levelLogs, LogMock{Msg: msg, Args: args})
+			logger.Log(hclog.Level(level+1), msg, args...)
+		}
+		logs[level] = levelLogs
 	}
 
 	for level, levelLogs := range logs {

ea68fe9d48beec654d6f0022c860cfd710e973c3
Yury Frolov <yura200253@gmail.com>
2021-08-02 15:36:24 +0500

    Formatted

--- storage/clickhousespanstore/mocks/converter_test.go
+++ storage/clickhousespanstore/mocks/converter_test.go
@@ -2,10 +2,11 @@ package mocks
 
 import (
 	"database/sql/driver"
-	"github.com/jaegertracing/jaeger/model"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/jaegertracing/jaeger/model"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConverterMock_ConvertValue(t *testing.T) {
@@ -17,7 +18,7 @@ func TestConverterMock_ConvertValue(t *testing.T) {
 	}
 
 	testCases := map[string]test{
-		"string value": {"some string value", driver.Value("some string value")},
+		"string value":       {"some string value", driver.Value("some string value")},
 		"string slice value": {[]string{"some", "slice", "of", "strings"}, driver.Value("[some slice of strings]")},
 		"time value": {
 			time.Date(2002, time.February, 19, 14, 43, 51, 0, time.UTC),
@@ -27,10 +28,10 @@ func TestConverterMock_ConvertValue(t *testing.T) {
 			time.Unix(12340, 123456789).Sub(time.Unix(0, 0)),
 			driver.Value(int64(12340123456789)),
 		},
-		"int64 value": {int64(1823), driver.Value(int64(1823))},
-		"model.SpanID value": {model.SpanID(318148), driver.Value(model.SpanID(318148))},
+		"int64 value":         {int64(1823), driver.Value(int64(1823))},
+		"model.SpanID value":  {model.SpanID(318148), driver.Value(model.SpanID(318148))},
 		"model.TraceID value": {model.TraceID{Low: 0xabd5, High: 0xa31}, driver.Value("0000000000000a31000000000000abd5")},
-		"uint8 slice value": {[]uint8("asdkja"), driver.Value([]uint8{0x61, 0x73, 0x64, 0x6b, 0x6a, 0x61})},
+		"uint8 slice value":   {[]uint8("asdkja"), driver.Value([]uint8{0x61, 0x73, 0x64, 0x6b, 0x6a, 0x61})},
 	}
 
 	for name, test := range testCases {

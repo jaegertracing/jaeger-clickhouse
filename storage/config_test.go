f6e7f5062bd329745174d92e0ca5e7d9d719465d
Yury Frolov <yura200253@gmail.com>
2021-08-02 10:29:42 +0500

    Added new assertions to config test

--- storage/config_test.go
+++ storage/config_test.go
@@ -15,4 +15,7 @@ func TestSetDefaults(t *testing.T) {
 	assert.EqualValues(t, config.BatchWriteSize, defaultBatchSize, "Default batch write size is \"%d\", want \"%d\"", config.BatchWriteSize, defaultBatchSize)
 	assert.Equal(t, config.BatchFlushInterval, defaultBatchDelay, "Default batch flush size is \"%d\", want \"%d\"", config.BatchFlushInterval, defaultBatchDelay)
 	assert.Equal(t, config.MetricsEndpoint, defaultMetricsEndpoint, "Default metrics endpoint is \"%s\", want \"%s\"", config.MetricsEndpoint, defaultMetricsEndpoint)
+	assert.Equal(t, config.SpansTable, defaultSpansTable, "Default spans table name is is \"%s\", want \"%s\"", config.SpansTable, defaultSpansTable)
+	assert.Equal(t, config.SpansIndexTable, defaultSpansIndexTable, "Default index table name is is \"%s\", want \"%s\"", config.SpansIndexTable, defaultSpansIndexTable)
+	assert.Equal(t, config.OperationsTable, defaultOperationsTable, "Default operations table name is is \"%s\", want \"%s\"", config.OperationsTable, defaultOperationsTable)
 }

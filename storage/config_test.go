a33e53490fe01eea60da0508e3fc9ee6382c6a80
Yury Frolov <yura200253@gmail.com>
2021-08-02 10:26:20 +0500

    Refactored config test using assert

--- storage/config_test.go
+++ storage/config_test.go
@@ -1,27 +1,18 @@
 package storage
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestSetDefaults(t *testing.T) {
 	config := Configuration{}
 	config.setDefaults()
 
-	if config.Username != defaultUsername {
-		t.Errorf("Default username is \"%s\", want \"%s\"", config.Database, defaultUsername)
-	}
-	if config.Database != defaultDatabaseName {
-		t.Errorf("Default database name is \"%s\", want \"%s\"", config.Database, defaultDatabaseName)
-	}
-	if config.Encoding != defaultEncoding {
-		t.Errorf("Default encoding is \"%s\", want \"%s\"", config.Encoding, defaultEncoding)
-	}
-	if config.BatchWriteSize != defaultBatchSize {
-		t.Errorf("Default batch write size is \"%d\", want \"%d\"", config.BatchWriteSize, defaultBatchSize)
-	}
-	if config.BatchFlushInterval != defaultBatchDelay {
-		t.Errorf("Default batch flush size is \"%d\", want \"%d\"", config.BatchFlushInterval, defaultBatchDelay)
-	}
-	if config.MetricsEndpoint != defaultMetricsEndpoint {
-		t.Errorf("Default metrics endpoint is \"%s\", want \"%s\"", config.MetricsEndpoint, defaultMetricsEndpoint)
-	}
+	assert.Equal(t, config.Username, defaultUsername, "Default username is \"%s\", want \"%s\"", config.Database, defaultUsername)
+	assert.Equal(t, config.Database, defaultDatabaseName, "Default database name is \"%s\", want \"%s\"", config.Database, defaultDatabaseName)
+	assert.Equal(t, config.Encoding, defaultEncoding, "Default encoding is \"%s\", want \"%s\"", config.Encoding, defaultEncoding)
+	assert.EqualValues(t, config.BatchWriteSize, defaultBatchSize, "Default batch write size is \"%d\", want \"%d\"", config.BatchWriteSize, defaultBatchSize)
+	assert.Equal(t, config.BatchFlushInterval, defaultBatchDelay, "Default batch flush size is \"%d\", want \"%d\"", config.BatchFlushInterval, defaultBatchDelay)
+	assert.Equal(t, config.MetricsEndpoint, defaultMetricsEndpoint, "Default metrics endpoint is \"%s\", want \"%s\"", config.MetricsEndpoint, defaultMetricsEndpoint)
 }

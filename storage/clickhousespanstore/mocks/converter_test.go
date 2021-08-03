916c24656b861f07673b3bcf8023c4fd10cb1c79
Yury Frolov <yura200253@gmail.com>
2021-08-03 17:25:36 +0500

    Added param names to converter mock tests

--- storage/clickhousespanstore/mocks/converter_test.go
+++ storage/clickhousespanstore/mocks/converter_test.go
@@ -16,20 +16,20 @@ func TestConverterMock_ConvertValue(t *testing.T) {
 		valueToConvert interface{}
 		expectedResult driver.Value
 	}{
-		"string value":       {"some string value", driver.Value("some string value")},
-		"string slice value": {[]string{"some", "slice", "of", "strings"}, driver.Value("[some slice of strings]")},
+		"string value":       {valueToConvert: "some string value", expectedResult: driver.Value("some string value")},
+		"string slice value": {valueToConvert: []string{"some", "slice", "of", "strings"}, expectedResult: driver.Value("[some slice of strings]")},
 		"time value": {
-			time.Date(2002, time.February, 19, 14, 43, 51, 0, time.UTC),
-			driver.Value(time.Date(2002, time.February, 19, 14, 43, 51, 0, time.UTC)),
+			valueToConvert: time.Date(2002, time.February, 19, 14, 43, 51, 0, time.UTC),
+			expectedResult: driver.Value(time.Date(2002, time.February, 19, 14, 43, 51, 0, time.UTC)),
 		},
 		"duration value": {
-			time.Unix(12340, 123456789).Sub(time.Unix(0, 0)),
-			driver.Value(int64(12340123456789)),
+			valueToConvert: time.Unix(12340, 123456789).Sub(time.Unix(0, 0)),
+			expectedResult: driver.Value(int64(12340123456789)),
 		},
-		"int64 value":         {int64(1823), driver.Value(int64(1823))},
-		"model.SpanID value":  {model.SpanID(318148), driver.Value(model.SpanID(318148))},
-		"model.TraceID value": {model.TraceID{Low: 0xabd5, High: 0xa31}, driver.Value("0000000000000a31000000000000abd5")},
-		"uint8 slice value":   {[]uint8("asdkja"), driver.Value([]uint8{0x61, 0x73, 0x64, 0x6b, 0x6a, 0x61})},
+		"int64 value":         {valueToConvert: int64(1823), expectedResult: driver.Value(int64(1823))},
+		"model.SpanID value":  {valueToConvert: model.SpanID(318148), expectedResult: driver.Value(model.SpanID(318148))},
+		"model.TraceID value": {valueToConvert: model.TraceID{Low: 0xabd5, High: 0xa31}, expectedResult: driver.Value("0000000000000a31000000000000abd5")},
+		"uint8 slice value":   {valueToConvert: []uint8("asdkja"), expectedResult: driver.Value([]uint8{0x61, 0x73, 0x64, 0x6b, 0x6a, 0x61})},
 	}
 
 	for name, test := range testCases {

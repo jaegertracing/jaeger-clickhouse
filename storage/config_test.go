7c7370727382defd2b6372bc6099773d698edbd9
Yury Frolov <yura200253@gmail.com>
2021-08-03 14:35:34 +0500

    Formatted config test

--- storage/config_test.go
+++ storage/config_test.go
@@ -10,18 +10,18 @@ import (
 func TestSetDefaults(t *testing.T) {
 	config := Configuration{}
 	config.setDefaults()
-	tests := map[string]struct{
-		field interface{}
+	tests := map[string]struct {
+		field    interface{}
 		expected interface{}
-	} {
-		"username": {config.Username, defaultUsername},
-		"database name": {config.Database, defaultDatabaseName},
-		"encoding": {config.Encoding, defaultEncoding},
-		"batch write size": {config.BatchWriteSize, defaultBatchSize},
-		"batch flush interval": {config.BatchFlushInterval, defaultBatchDelay},
-		"metrics endpoint": {config.MetricsEndpoint, defaultMetricsEndpoint},
-		"spans table name": {config.SpansTable, defaultSpansTable},
-		"index table name": {config.SpansIndexTable, defaultSpansIndexTable},
+	}{
+		"username":              {config.Username, defaultUsername},
+		"database name":         {config.Database, defaultDatabaseName},
+		"encoding":              {config.Encoding, defaultEncoding},
+		"batch write size":      {config.BatchWriteSize, defaultBatchSize},
+		"batch flush interval":  {config.BatchFlushInterval, defaultBatchDelay},
+		"metrics endpoint":      {config.MetricsEndpoint, defaultMetricsEndpoint},
+		"spans table name":      {config.SpansTable, defaultSpansTable},
+		"index table name":      {config.SpansIndexTable, defaultSpansIndexTable},
 		"operations table name": {config.OperationsTable, defaultOperationsTable},
 	}
 

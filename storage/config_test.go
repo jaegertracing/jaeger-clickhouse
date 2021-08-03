75d506e8c10fb9ec9dcd0af9374d3562494ca04b
Yury Frolov <yura200253@gmail.com>
2021-08-03 14:24:41 +0500

    Another fix of config test

--- storage/config_test.go
+++ storage/config_test.go
@@ -14,7 +14,7 @@ func TestSetDefaults(t *testing.T) {
 	assert.Equal(t, defaultDatabaseName, config.Database, "Incorrect default database name")
 	assert.Equal(t, defaultEncoding, config.Encoding, "Incorrect default encoding")
 	assert.EqualValues(t, defaultBatchSize, config.BatchWriteSize, "Incorrect default batch write size")
-	assert.Equal(t, defaultBatchDelay, config.BatchFlushInterval, "Incorrect default batch flush size")
+	assert.Equal(t, defaultBatchDelay, config.BatchFlushInterval, "Incorrect default batch flush interval")
 	assert.Equal(t, defaultMetricsEndpoint, config.MetricsEndpoint, "Incorrect default metrics endpoint")
 	assert.Equal(t, defaultSpansTable, config.SpansTable, "Incorrect default spans table name")
 	assert.Equal(t, defaultSpansIndexTable, config.SpansIndexTable, "Incorrect default index table name")

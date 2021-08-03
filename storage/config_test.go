ed0878844ce717fade631bc0d91093f779cb6be5
Yury Frolov <yura200253@gmail.com>
2021-08-03 14:14:56 +0500

    Fixed & rewrited config test

--- storage/config_test.go
+++ storage/config_test.go
@@ -10,13 +10,13 @@ func TestSetDefaults(t *testing.T) {
 	config := Configuration{}
 	config.setDefaults()
 
-	assert.Equal(t, config.Username, defaultUsername, "Default username is \"%s\", want \"%s\"", config.Database, defaultUsername)
-	assert.Equal(t, config.Database, defaultDatabaseName, "Default database name is \"%s\", want \"%s\"", config.Database, defaultDatabaseName)
-	assert.Equal(t, config.Encoding, defaultEncoding, "Default encoding is \"%s\", want \"%s\"", config.Encoding, defaultEncoding)
-	assert.EqualValues(t, config.BatchWriteSize, defaultBatchSize, "Default batch write size is \"%d\", want \"%d\"", config.BatchWriteSize, defaultBatchSize)
-	assert.Equal(t, config.BatchFlushInterval, defaultBatchDelay, "Default batch flush size is \"%d\", want \"%d\"", config.BatchFlushInterval, defaultBatchDelay)
-	assert.Equal(t, config.MetricsEndpoint, defaultMetricsEndpoint, "Default metrics endpoint is \"%s\", want \"%s\"", config.MetricsEndpoint, defaultMetricsEndpoint)
-	assert.Equal(t, config.SpansTable, defaultSpansTable, "Default spans table name is is \"%s\", want \"%s\"", config.SpansTable, defaultSpansTable)
-	assert.Equal(t, config.SpansIndexTable, defaultSpansIndexTable, "Default index table name is is \"%s\", want \"%s\"", config.SpansIndexTable, defaultSpansIndexTable)
-	assert.Equal(t, config.OperationsTable, defaultOperationsTable, "Default operations table name is is \"%s\", want \"%s\"", config.OperationsTable, defaultOperationsTable)
+	assert.Equal(t, defaultUsername, config.Username, "Incorrect default username")
+	assert.Equal(t, defaultDatabaseName, config.Database, "Incorrect default database name")
+	assert.Equal(t, defaultEncoding, config.Encoding, "Incorrect default encoding")
+	assert.EqualValues(t, defaultBatchSize, config.BatchWriteSize, "Incorrect default batch write size")
+	assert.Equal(t, defaultBatchDelay, config.BatchFlushInterval, "Incorrect default batch flush size")
+	assert.Equal(t, defaultMetricsEndpoint, config.MetricsEndpoint, "Incorrect default metrics endpoint")
+	assert.Equal(t, defaultSpansTable, config.SpansTable, "Incorrect default spans table name")
+	assert.Equal(t, defaultSpansIndexTable, config.SpansIndexTable, "Incorrect default index table name")
+	assert.Equal(t, defaultOperationsTable, config.OperationsTable, "Incorrect default operations table name")
 }

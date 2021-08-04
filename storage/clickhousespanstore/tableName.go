048849edc068ce4c5e705fd999b36f82d448d72b
Yury Frolov <yura200253@gmail.com>
2021-08-04 17:47:04 +0500

    Made TableName methods global

--- storage/clickhousespanstore/tableName.go
+++ storage/clickhousespanstore/tableName.go
@@ -6,10 +6,10 @@ const databaseName = "jaeger"
 
 type TableName string
 
-func (tableName TableName) toGlobal() TableName {
+func (tableName TableName) ToGlobal() TableName {
 	return tableName[:len(tableName)-6]
 }
 
-func (tableName TableName) addDbName() TableName {
+func (tableName TableName) AddDbName() TableName {
 	return TableName(fmt.Sprintf("%s_%s", databaseName, tableName))
 }

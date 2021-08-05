5315ca796fcb2a80540f8eb6e0e74467e3a4a044
Yury Frolov <yura200253@gmail.com>
2021-08-05 14:13:55 +0500

    Added tests for SpanReader.getStrings
    Signed-off-by: Yury Frolov <yura200253@gmail.com>

--- storage/clickhousespanstore/reader_test.go
+++ storage/clickhousespanstore/reader_test.go
@@ -2,6 +2,9 @@ package clickhousespanstore
 
 import (
 	"context"
+	"database/sql/driver"
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jaegertracing/jaeger/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,6 +15,8 @@ import (
 const (
 	testOperationsTable = "test_operations_table"
 )
+
+var errorMock = fmt.Errorf("error mock")
 
 func TestSpanReader_findTraceIDsInRangeNoIndexTable(t *testing.T) {
 	db, _, err := getDbMock()
@@ -45,4 +50,67 @@ func TestSpanReader_findTraceIDsInRangeEndBeforeStart(t *testing.T) {
 	)
 	assert.Equal(t, make([]model.TraceID, 0), res)
 	assert.NoError(t, err)
+}
+
+func TestSpanReader_getStrings(t *testing.T) {
+	db, mock, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	query := "SELECT b FROM a WHERE b != ?"
+	argValues := []driver.Value{driver.Value("a")}
+	args := []interface{}{"a"}
+	rows := []driver.Value{"some", "query", "rows"}
+	expectedResult := []string{"some", "query", "rows"}
+	result := sqlmock.NewRows([]string{"b"})
+	for _, str := range rows {
+		result.AddRow(str)
+	}
+	mock.ExpectQuery(query).WithArgs(argValues...).WillReturnRows(result)
+
+	traceReader := NewTraceReader(db, testOperationsTable, testIndexTable, testSpansTable)
+
+	queryResult, err := traceReader.getStrings(context.Background(), query, args...)
+	assert.NoError(t, err)
+	assert.EqualValues(t, expectedResult, queryResult)
+}
+
+func TestSpanReader_getStringsQueryError(t *testing.T) {
+	db, mock, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	query := "SELECT b FROM a WHERE b != ?"
+	argValues := []driver.Value{driver.Value("a")}
+	args := []interface{}{"a"}
+	mock.ExpectQuery(query).WithArgs(argValues...).WillReturnError(errorMock)
+
+	traceReader := NewTraceReader(db, testOperationsTable, testIndexTable, testSpansTable)
+
+	queryResult, err := traceReader.getStrings(context.Background(), query, args...)
+	assert.EqualError(t, err, errorMock.Error())
+	assert.EqualValues(t, []string(nil), queryResult)
+}
+
+func TestSpanReader_getStringsRowError(t *testing.T) {
+	db, mock, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	query := "SELECT b FROM a WHERE b != ?"
+	argValues := []driver.Value{driver.Value("a")}
+	args := []interface{}{"a"}
+	rows := []driver.Value{"some", "query", "rows"}
+	result := sqlmock.NewRows([]string{"b"})
+	for _, str := range rows {
+		result.AddRow(str)
+	}
+	result.RowError(2, errorMock)
+	mock.ExpectQuery(query).WithArgs(argValues...).WillReturnRows(result)
+
+	traceReader := NewTraceReader(db, testOperationsTable, testIndexTable, testSpansTable)
+
+	queryResult, err := traceReader.getStrings(context.Background(), query, args...)
+	assert.EqualError(t, err, errorMock.Error())
+	assert.EqualValues(t, []string(nil), queryResult)
 }

43fef703a2401d41539dc98e42cd74b225f84afa
Yury Frolov <yura200253@gmail.com>
2021-08-09 16:44:57 +0500

    Added tests for TableName
    Signed-off-by: Yury Frolov <yura200253@gmail.com>

---
+++ storage/clickhousespanstore/tablename_test.go
@@ -0,0 +1,26 @@
+package clickhousespanstore
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTableName_AddDbName(t *testing.T) {
+	assert.Equal(t, TableName("database_name.table_name_local"), TableName("table_name_local").AddDbName("database_name"))
+}
+
+func TestTableName_ToGlobal(t *testing.T) {
+	tests := map[string]struct{
+		tableName TableName
+		expected TableName
+	} {
+		"trailing '_local'": {tableName: "table_name_local", expected: "table_name"},
+		"internal '_local'": {tableName: "table_name_local_suffix", expected: "table_name_suffix"},
+		"no '_local'": {tableName: "table_name", expected: "table_name"},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.tableName.ToGlobal())
+		})
+	}
+}

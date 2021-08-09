9f996cb82add28b71c64b3695b61ba33e8edddb0
Yury Frolov <yura200253@gmail.com>
2021-08-09 17:49:38 +0500

    Added TableName.ToLocal method

--- storage/clickhousespanstore/tablename.go
+++ storage/clickhousespanstore/tablename.go
@@ -11,6 +11,10 @@ func (tableName TableName) ToGlobal() TableName {
 	return TableName(strings.ReplaceAll(string(tableName), "_local", ""))
 }
 
+func (tableName TableName) ToLocal() TableName {
+	return tableName + "_local"
+}
+
 func (tableName TableName) AddDbName(databaseName string) TableName {
 	return TableName(fmt.Sprintf("%s.%s", databaseName, tableName))
 }

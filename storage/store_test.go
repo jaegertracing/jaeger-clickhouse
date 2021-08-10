f597887a101ad858b98279197803ffe13ddbe446
Yury Frolov <yura200253@gmail.com>
2021-08-10 17:30:12 +0500

    Added tests for simple methods
    Signed-off-by: Yury Frolov <yura200253@gmail.com>

--- storage/store_test.go
+++ storage/store_test.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"database/sql"
 	"fmt"
+	"github.com/pavolloffay/jaeger-clickhouse/storage/clickhousedependencystore"
+	"github.com/pavolloffay/jaeger-clickhouse/storage/clickhousespanstore"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -13,7 +15,127 @@ import (
 	"github.com/pavolloffay/jaeger-clickhouse/storage/clickhousespanstore/mocks"
 )
 
+const (
+	testIndexTable        = "test_index_table"
+	testSpansTable        = "test_spans_table"
+	testOperationsTable   = "test_operation_table"
+	testSpansArchiveTable = "test_spans_archive_table"
+)
+
 var errorMock = fmt.Errorf("error mock")
+
+func TestStore_SpanWriter(t *testing.T) {
+	db, _, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	writer := clickhousespanstore.SpanWriter{}
+	store := Store{
+		db:     db,
+		writer: &writer,
+	}
+	assert.Equal(t, &writer, store.SpanWriter())
+}
+
+func TestStore_ArchiveSpanWriter(t *testing.T) {
+	db, _, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	writer := clickhousespanstore.SpanWriter{}
+	store := Store{
+		db:     db,
+		archiveWriter: &writer,
+	}
+	assert.Equal(t, &writer, store.ArchiveSpanWriter())
+}
+
+func TestStore_SpanReader(t *testing.T) {
+	db, _, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	reader := clickhousespanstore.TraceReader{}
+	store := Store{
+		db:     db,
+		reader: &reader,
+	}
+	assert.Equal(t, &reader, store.SpanReader())
+}
+
+func TestStore_ArchiveSpanReader(t *testing.T) {
+	db, _, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	reader := clickhousespanstore.TraceReader{}
+	store := Store{
+		db:     db,
+		archiveReader: &reader,
+	}
+	assert.Equal(t, &reader, store.ArchiveSpanReader())
+}
+
+func TestStore_DependencyReader(t *testing.T) {
+	db, _, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	logger := mocks.NewSpyLogger()
+	store := newStore(db, logger)
+	assert.Equal(t, &clickhousedependencystore.DependencyStore{}, store.DependencyReader())
+	logger.AssertLogsEmpty(t)
+}
+
+func TestStore_Close(t *testing.T) {
+	db, mock, err := getDbMock()
+	require.NoError(t, err, "an error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	logger := mocks.NewSpyLogger()
+	store := newStore(db, logger)
+
+	mock.ExpectClose()
+	require.NoError(t, store.Close())
+	assert.NoError(t, mock.ExpectationsWereMet())
+	logger.AssertLogsEmpty(t)
+}
+
+func newStore(db *sql.DB, logger mocks.SpyLogger) Store {
+	return Store{
+		db: db,
+		writer: clickhousespanstore.NewSpanWriter(
+			logger,
+			db,
+			testIndexTable,
+			testSpansTable,
+			clickhousespanstore.EncodingJSON,
+			0,
+			0,
+		),
+		reader: clickhousespanstore.NewTraceReader(
+			db,
+			testOperationsTable,
+			testIndexTable,
+			testSpansTable,
+		),
+		archiveWriter: clickhousespanstore.NewSpanWriter(
+			logger,
+			db,
+			testIndexTable,
+			testSpansArchiveTable,
+			clickhousespanstore.EncodingJSON,
+			0,
+			0,
+		),
+		archiveReader: clickhousespanstore.NewTraceReader(
+			db,
+			testOperationsTable,
+			testIndexTable,
+			testSpansArchiveTable,
+			),
+	}
+}
 
 func TestStore_executeScripts(t *testing.T) {
 	db, mock, err := getDbMock()

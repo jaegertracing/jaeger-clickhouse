724192b43f691fe754236a50af417a2857856a3c
Yury Frolov <yura200253@gmail.com>
2021-08-11 10:26:15 +0500

    Remoced test for ToGlobal & added test for ToLocal
    Signed-off-by: Yury Frolov <yura200253@gmail.com>

--- storage/clickhousespanstore/tablename_test.go
+++ storage/clickhousespanstore/tablename_test.go
@@ -10,18 +10,8 @@ func TestTableName_AddDbName(t *testing.T) {
 	assert.Equal(t, TableName("database_name.table_name_local"), TableName("table_name_local").AddDbName("database_name"))
 }
 
-func TestTableName_ToGlobal(t *testing.T) {
-	tests := map[string]struct {
-		tableName TableName
-		expected  TableName
-	}{
-		"trailing '_local'": {tableName: "table_name_local", expected: "table_name"},
-		"internal '_local'": {tableName: "table_name_local_suffix", expected: "table_name_suffix"},
-		"no '_local'":       {tableName: "table_name", expected: "table_name"},
-	}
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			assert.Equal(t, test.expected, test.tableName.ToGlobal())
-		})
-	}
+func TestTableName_ToLocal(t *testing.T) {
+	var tableName TableName = "some_table"
+	assert.Equal(t, tableName + "_local", tableName.ToLocal())
+
 }

311666dc934483def3d2fe292c1de640b33d4160
Yury Frolov <yura200253@gmail.com>
2021-08-20 10:55:23 +0500

    Changed global init scripts
    Signed-off-by: Yury Frolov <yura200253@gmail.com>

--- sqlscripts/replication/0003-jaeger-operations-local.sql
+++ sqlscripts/replication/0003-jaeger-operations-local.sql
@@ -4,9 +4,10 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS %s ON CLUSTER '{cluster}'
             PARTITION BY toYYYYMM(date) ORDER BY (date, service, operation)
             SETTINGS index_granularity=32
         POPULATE
-AS SELECT toDate(timestamp) AS date,
+AS SELECT toDate(timestamp)                                                              AS date,
           service,
           operation,
-          count()           as count
+          count()                                                                        as count,
+          if(has(tag_keys, 'span.kind'), tag_values[indexOf(tag_keys, 'span.kind')], '') as spankind
    FROM %s -- here goes local index table
-   GROUP BY date, service, operation;
+   GROUP BY date, service, operation, tag_keys, tag_values;
